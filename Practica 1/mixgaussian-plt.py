4da483e230fcbc2cc6bab42dad00d13f1b8abb68
HakuMi <82086904+mire4@users.noreply.github.com>
2022-11-19 01:29:20 +0100

    Primera actualizacion

---
+++ Practica 1/mixgaussian-plt.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+data=np.loadtxt('mixgaussian-exp.out')
+fig, ax = plt.subplots()
+ax.set_title('MNIST con 90% entrenamiento y 10% validacion');
+ax.grid();
+ax.set_xlabel('Numero de componentes');
+ax.set_xlim([0.9,220]);
+ax.set_xscale('log');
+ax.set_xticks([1,2,5,10,20,50,100,200])
+ax.set_xticklabels([1,2,5,10,20,50,100,200])
+ax.set_ylim([0,6]);
+ax.set_ylabel('error de clasificacion');
+ax.set_yticks(range(7))
+ax.plot(data[:,0], data[:,1], label='tr', lw=2, marker='o', markersize=10)
+ax.plot(data[:,0], data[:,2], label='dv', lw=2, marker='x', markersize=10)
+ax.legend();
+plt.savefig('mixgaussian-K.pdf');
+plt.show();
